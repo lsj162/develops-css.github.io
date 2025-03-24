9c5d452fbc6de9fb99202dc5fca9a25a9b6aa64f
global name <global email>
2025-03-24 09:31:00 +0800

    fix: 修改

--- assets/index-CIBK4Ym3.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DOTfErbM.js";const s="_fixed_mm81h_1",i={fixed:s};function x(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:i.fixed,children:"fixed"})})}export{x as default};
