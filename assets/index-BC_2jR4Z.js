76d9e533e02981dd29abf2808c239d5895c3ef37
global name <global email>
2025-04-11 08:15:28 +0800

    fix: 修改

---
+++ assets/index-BC_2jR4Z.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B-fC14-H.js";function t(){return e.jsx(e.Fragment,{children:e.jsx("h1",{children:"pointer-events"})})}export{t as default};
--- assets/index-CCBfj9KI.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-B5QjSDPG.js";const e="_triangle_fwrfk_1",r={triangle:e};function s(){return t.jsx(t.Fragment,{children:t.jsx("div",{className:`${r.triangle}`})})}export{s as default};
